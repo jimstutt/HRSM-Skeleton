a0e9f10fae884b774af4a0c8e7d6bb256412f117
Jim Stuttard <jim.a.stuttard@gmail.com>
2025-11-14 19:23:28 +0000

    Use fetchTarball for WASI SDK (fix GitHub 404)

--- pkgs/wasm32-wasi-ghc-full.nix
+++ pkgs/wasm32-wasi-ghc-full.nix
@@ -1,17 +1,16 @@
 { pkgs }:
 
 let
-  inherit (pkgs) stdenv fetchurl lib;
-
   wasiSdkVersion = "20.0";
   wasiSdkTag     = "wasi-sdk-20";
 
-  wasiSdkSrc = fetchurl {
+  # Use fetchTarball because GitHub blocks fetchurl/curl without User-Agent
+  wasiSdkSrc = builtins.fetchTarball {
     url = "https://github.com/WebAssembly/wasi-sdk/releases/download/${wasiSdkTag}/wasi-sdk-${wasiSdkVersion}-x86_64-linux.tar.xz";
-    sha256 = "sha256-xlS4h3yfBn35qSdz8sRP/6IDdnh3oX1N1pAVccawJg4=";
+    sha256 = "1v4n9i6r9snyrxck4rc9dicl7818apmhklg6f7w9qjidv3rj8ci";
   };
 
-in stdenv.mkDerivation {
+in pkgs.stdenv.mkDerivation {
   pname = "ghc-wasm32-wasi-env";
   version = "9.10.1";
 
@@ -26,20 +25,15 @@ in stdenv.mkDerivation {
 
   installPhase = ''
     mkdir -p $out/wasi-sdk
-    tar -xJf ${wasiSdkSrc} -C $out/wasi-sdk --strip-components=1
+    tar -xf ${wasiSdkSrc} -C $out/wasi-sdk --strip-components=1
 
     mkdir -p $out/bin
-    ln -sf ${pkgs.clang}/bin/clang     $out/bin/clang
-    ln -sf ${pkgs.clang}/bin/clang++   $out/bin/clang++
-    ln -sf ${pkgs.lld}/bin/wasm-ld     $out/bin/wasm-ld
+    ln -sf ${pkgs.clang}/bin/clang   $out/bin/clang
+    ln -sf ${pkgs.clang}/bin/clang++ $out/bin/clang++
+    ln -sf ${pkgs.lld}/bin/wasm-ld  $out/bin/wasm-ld
 
     mkdir -p $out/share
     echo "$out/wasi-sdk/share/wasi-sysroot" > $out/share/wasi-sysroot-path
   '';
-
-  meta = {
-    description = "WASM32 WASI env for GHC with WASI SDK";
-    license = lib.licenses.bsd3;
-    platforms = [ "x86_64-linux" ];
-  };
 }
+
