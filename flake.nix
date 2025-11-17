f1ab906a77f3f3da82eb9b4d1c1b733a50b6039d
Jim Stuttard <jim.a.stuttard@gmail.com>
2025-11-17 12:53:00 +0000

    Regenerate NGOLogisticsCG flake

--- flake.nix
+++ flake.nix
@@ -1,42 +1,59 @@
 {
-  description = "NGO Logistics Dev Environment with WASM32-WASI GHC";
+  description = "NGOLogisticsCG WASM/Web + DevShell";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-
-        # Import your wasm env derivation directly
-        wasmGhcEnv = import ./pkgs/wasm32-wasi-ghc-full.nix { inherit pkgs; };
-
+        pkgs = import nixpkgs { inherit system; };  
       in {
+
+        # -------------------------------
+        # Development shell for WASM work
+        # -------------------------------
         devShells.default = pkgs.mkShell {
-          name = "ngologisticscg-dev-env";
+          name = "NGOLogisticsCG";
 
-          buildInputs = [
-            pkgs.cabal-install
-            pkgs.haskell.compiler.ghc9101
+          buildInputs = with pkgs; [
+            # Haskell Reflex Platform
+            reflex-platform
 
-            # wasm32 env you defined
-            wasmGhcEnv
+            # Web/WASM build toolchain
+            nodejs_20
+            wasm-pack
+            binaryen
+            emscripten
 
-            # Useful extras
-            pkgs.binaryen
-            pkgs.wasmtime
+            # Tools
+            git
+            curl
+            wget
+            typescript
+            vim
           ];
 
           shellHook = ''
-            echo "WASM32 GHC + WASI SDK available"
-            echo "Sysroot: $(cat ${wasmGhcEnv}/share/wasi-sysroot-path)"
+            echo "[NGOLogisticsCG] WASM + Reflex Development Environment"
+            echo "cd ~/Dev/NGOLogisticsCG && ./scripts/build.sh"
           '';
         };
-      }
-    );
+
+        # -------------------------------
+        # Packages (if you later build a binary)
+        # -------------------------------
+        packages = { };
+
+        # -------------------------------
+        # Apps (nix run)
+        # -------------------------------
+        apps.default = {
+          type = "app";
+          program = ''${pkgs.bash}/bin/bash -c "cd $PWD && npm run dev"'';
+        };
+
+      });
 }
