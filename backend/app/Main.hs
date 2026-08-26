a449f1823b79bde78280841c521b2ab6307d4a3f
jimastutt <jimastuttard@gmail.com>
2026-08-26 20:57:13 +0100

    [HRSM] Use dedicated MariaDB user to bypass root socket auth

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -3,14 +3,15 @@ module Main where
 import Backend (app)
 import Network.Wai.Handler.Warp (run)
 import qualified Database.MySQL.Simple as MySQL
-import DB (DBConn)
 
 main :: IO ()
 main = do
   putStrLn "[HRSM] Backend connecting to MariaDB (project_db)..."
   conn <- MySQL.connect MySQL.defaultConnectInfo 
     { MySQL.connectDatabase = "project_db"
-    , MySQL.connectUser = "root"
+    , MySQL.connectUser = "hrsm_user"
+    , MySQL.connectPassword = "hrsm_password"
+    , MySQL.connectHost = "127.0.0.1" -- Force TCP to bypass unix_socket auth
     }
   
   putStrLn "[HRSM] Backend starting on port 8080..."
